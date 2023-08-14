e6c18f06412af8d632910bc036e548cb816f621f
coding-to-music <connors.tom@gmail.com>
2023-08-13 22:47:54 -0500

    Build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { Analytics } from "@vercel/analytics/react"
 export default function IndexPage() {
   return (
     <section className="container grid h-full items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex flex-col h-full max-w-[980px] items-center justify-center mx-auto">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl mb-5">
+      <div className="mx-auto flex h-full max-w-[980px] flex-col items-center justify-center">
+        <h1 className="mb-5 text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Get awesome stuff to your inbox!
         </h1>
         <SignupForm />
